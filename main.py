efaf33a9d4d9fc1b14da9adb4045baff7205dafa
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2023-12-03 22:17:29 +0530

    First

---
+++ main.py
@@ -0,0 +1,79 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from math import floor
+
+#  CONSTANT
+
+PINK = "#E2979C"
+RED = "#E7305B"
+GREEN = "#9BDEAC"
+YELLOW = "#F7F5DD"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+
+# TIMER RESET
+
+
+
+# TIMER MECHANISM
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = 5
+    short_break_sec = 2
+    long_break_sec = LONG_BREAK_MIN * 60
+    
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break" , fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break" , fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work" , fg=GREEN)
+
+# COUNTDOWN MECHANISM
+
+def count_down(count):
+    count_min = floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f'0{count_sec}'
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(floor(reps/2)):
+            mark += "✔"
+        check_label.config(text=mark)
+
+# UI SETUP
+
+window = Tk()
+window.title("Pomodoro Project")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME,45,"bold"))
+timer_label.grid(column=1, row=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+image = Image.open("tomato.png")
+image = image.resize((200,223))
+tomato_img = ImageTk.PhotoImage(image)
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 120, text="00:00", fill="white", font=(FONT_NAME,30,"bold"))
+canvas.grid(column=1,row=1)
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(column=0,row=2)
+check_label = Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME,16,"bold"))
+check_label.grid(column=1, row=3)
+reset_button = Button(text="Reset")
+reset_button.grid(column=2,row=2)
+
+window.mainloop()
